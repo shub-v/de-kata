48a49969bf01c9141252b6d25f66fd1f2d2cdf91
Shubhank Vijayvergiya <shubhankvijayvergiya@Shubhanks-MBP.lan>
2024-10-05 15:11:51 +1000

    improvements

--- dbt/models/staging/stg__categories.sql
+++ dbt/models/staging/stg__categories.sql
@@ -3,43 +3,36 @@
 ) }}
 
 with source as (
-    select *
-    from {{ source('raw_layer', 'de_test_categories') }}
-
-),
-
-clean_source as (
-
-
     select
         id,
         disabled,
-        case when lower(parent_id) = 'null' then null else parent_id end
-            as parent_id
+        parent_id
+    from {{ source('raw_layer', 'de_test_categories') }}
+),
+
+clean_source as (
+    select
+        id,
+        disabled,
+        NULLIF(lower(trim(parent_id)), 'null') as parent_id
     from source
 ),
 
 parent_disabled_status as (
     select
-        c1.id,
-        c1.parent_id,
-        c1.disabled,
-        c2.disabled as parent_disabled
-    from clean_source as c1
-    left join clean_source as c2 on c1.parent_id = c2.id
+        child.id,
+        child.parent_id,
+        child.disabled,
+        parent.disabled as parent_disabled
+    from clean_source as child
+    left join clean_source as parent on child.parent_id = parent.id
 ),
 
 renamed as (
     select
         id,
         parent_id,
-        case
-            when
-                disabled is null and parent_disabled is not null
-                then parent_disabled
-            else disabled
-        end as disabled
-
+        coalesce(disabled, parent_disabled) as disabled
     from parent_disabled_status
 )
 
